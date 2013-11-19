c43e74a634e752dc7f228ec17fe32f0c7f8fe86b
root <root@localhost.localdomain>
2013-11-19 16:41:16 +0800

    install fedora in virtualbox

--- install-pip.sh
+++ install-pip.sh
@@ -2,3 +2,6 @@ curl -O http://python-distribute.org/distribute_setup.py
 curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 python distribute_setup.py
 python get-pip.py
+rm distribute* -rf
+rm get-pip.py -rf
+
---
+++ install-py-venv.sh
@@ -0,0 +1,4 @@
+pip install virtualenv
+pip install pythonbrew
+#yum -y groupinstall "Development Libraries"
+#yum -y groupinstall "Development Tools"
